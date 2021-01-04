f4fe784a4395e790f561acec48d71068dc0014b3
adich <adi.ch2019@outlook.com>
2021-01-05 04:44:29 +0530

    Initial Commit

---
+++ src/ECommerce/ECommerce.Api.Products.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ECommerce.Api.Products.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/ECommerce/ECommerce.Api.Products/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsProvider productsProvider;
+
+        public ProductsController(IProductsProvider productsProvider)
+        {
+            this.productsProvider = productsProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            var all = await productsProvider.GetProductsAsync();
+            if (all.IsSuccess)
+            {
+                return Ok(all.Products);
+            }
+            return NotFound(all.ErrorMessage);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductAsync(int id)
+        {
+            var result = await productsProvider.GetProductAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Product);
+            }
+            return NotFound(result.ErrorMessage);
+        }
+    }
+}
